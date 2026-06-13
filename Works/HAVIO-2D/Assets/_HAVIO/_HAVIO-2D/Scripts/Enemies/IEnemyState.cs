52b57cf477efb4ba0d064e196a74f58052b8802e
Human3069 <108985067+Human3069@users.noreply.github.com>
2026-06-13 11:20:17 +0900

    remove directory
    remove unnecessary directory

--- Works/HAVIO-2D/Assets/_HAVIO/_HAVIO-2D/Scripts/Enemies/IEnemyState.cs
+++
@@ -1,9 +0,0 @@
-namespace HAVIO
-{
-    public interface IEnemyState
-    {
-        void Enter(EnemyContext context, EnemyData data);
-        void Exit();
-        void SlowTick(); // 0.5초마다 호출
-    }
-}
